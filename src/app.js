48baaa1e056f4c4d469f1dcd3e02e0ffab94a396
WyattChase <esnaruto@yahoo.com>
2023-02-06 22:35:06 +0000

    final

--- src/app.js
+++ src/app.js
@@ -8,4 +8,17 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  document.querySelector("#domain").innerHTML = generateExcuse();
+  console.log(generateExcuse);
+};
+let generateExcuse = () => {
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+
+  let whoPronoun = Math.floor(Math.random() * pronoun.length);
+  let actAdj = Math.floor(Math.random() * adj.length);
+  let whatNoun = Math.floor(Math.random() * noun.length);
+
+  return pronoun[whoPronoun] + adj[actAdj] + noun[whatNoun] + ".com";
 };
